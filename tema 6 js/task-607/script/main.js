246b4fc3bbcf7d61920655dae208867976ae72e4
MFkirin <119575220+MFkirin@users.noreply.github.com>
2023-03-23 13:42:59 +0100

    Add files via upload

--- tema 6 js/task-607/script/main.js
+++ tema 6 js/task-607/script/main.js
@@ -3,38 +3,56 @@ const books = [
     ["The Catcher in the Rye", "J.D. Salinger", "1951"],
     ["To Kill a Mockingbird", "Harper Lee", "1960"],
     ["1984", "George Orwell", "1949"]
-  ];
+];
 
-console.log(books[0][0]); // "The Catcher in the Rye"
-console.log(books[1][1]); // "Harper Lee"
-console.log(books[2][2]); // "1949"
-    
 function createTable(books) {
     let table = document.createElement('table');
     let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
     let tr = document.createElement('tr');
+
     const ths = ["Id", "Title", "Author", "Year", "Action"]
+
     // TODO: create a table based on the array received as a parameter
 
-    for(let i= 0; i<5; i++){
+    for (let i = 0; i < 5; i++) {
         let th = document.createElement('th');
         th.textContent = ths[i];
-        tr.appendChild(th); 
+        tr.append(th);
     }
-    thead.appendChild(tr);
-    table.appendChild(thead)
+    thead.append(tr);
+
+
+    for (let i = 0; i < books.length; i++) {
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        td.textContent = [i];
+        tr.appendChild(td);
+        for (let j = 0; j < books.length; j++) {
+            let td = document.createElement('td');
+            td.textContent = books[i][j];
+            tr.appendChild(td);
+        }
+        let deleteButton = document.createElement('button');
+        let tdDelete = document.createElement('td');
+        deleteButton.textContent = "Delete";
+        tr.appendChild(deleteButton);
+        
+        tbody.appendChild(tr);
+    }
+
+    table.append(thead);
+    table.append(tbody);
     return table;
 }
 
 function deleteTable() {
     let table = document.querySelector('table');
 
-    tbody 
-    tr
-    td
+
 
     // TODO: delete the table
-    
+
 }
 
 function newBook() {
